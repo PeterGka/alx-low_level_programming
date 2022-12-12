4431002535f17df04042be6df183518e0432c1be
PeterGka <kymnjoro254@gmail.com>
2022-12-11 17:25:16 -0800

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
-*main entry
-*return point
-*/
+ *main-printf using printf 
+ *return:0
+ */
 int main(void)
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
