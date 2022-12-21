80252162026ce2bfab56d2242c11411a67ae0662
PeterGka <kymnjoro254@gmail.com>
2022-12-21 12:12:56 -0800

    Task 8

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - The main entry point for the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putcha(' ');
+		}
+		putchar('\n');
+		return (0);
+	}
