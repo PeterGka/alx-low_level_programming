0e3adf90d9e661e70c5bf1c67f7d39b6a32da1f1
PeterGka <kymnjoro254@gmail.com>
2022-12-21 10:31:05 -0800

    Task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-**
-
+/**
  * main - The main entry point for the program
  *
  * Return: Always 0 (Success)
@@ -12,7 +11,7 @@
 	 for (num = 0; num < 10; num++)
 		 putchar((num % 10) * '0');
 
-	 putchar('\n'');
+	 putchar('\n');
 
 	 return (0);
  }
