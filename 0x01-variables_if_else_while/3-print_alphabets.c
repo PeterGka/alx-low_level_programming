88358c27f5d0acf6b18dd2cd51c7de887cdb8582
PeterGka <kymnjoro254@gmail.com>
2022-12-21 07:12:09 -0800

    Task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,7 +11,9 @@ int main(void)
 	for (ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);
 	for (ch = 'A'; ch <= 'Z'; ch++)
-		putchar(ch)
-			putchar('\n');
+		putchar(ch);
+
+	putchar('\n');
+
 	return (0);
 }
