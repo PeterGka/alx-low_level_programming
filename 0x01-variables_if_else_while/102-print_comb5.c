9c9c009e7dfe32987f5f4eed6c60bbab0db1f1f3
PeterGka <kymnjoro254@gmail.com>
2022-12-23 11:43:18 -0800

    Task 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - The main entry point for the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 100; i++)
+	{
+
+		for (int j = 0; j < 100; j++)
+		{
+
+			putchar(i / 10 + '0');
+
+			putchar(i % 10 + '0');
+
+			putchar(' ');
+
+			putchar(j / 10 + '0');
+
+			putchar(j % 10 + '0');
+
+			putchar(',');
+
+			putchar(' ');
+
+		}
+
+	}
+	return (0);
+}
