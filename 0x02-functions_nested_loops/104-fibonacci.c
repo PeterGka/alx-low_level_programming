84628f6e73925c600ce989364a7c3be256f51e10
PeterGka <kymnjoro254@gmail.com>
2022-12-26 06:25:57 -0800

    Task 15

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -52,27 +52,16 @@ int main(void)
 
 	aft2 = (aft % l);
 
-
 	for (i = 92; i < 99; ++i)
-
 	{
-
 		printf(", %lu", aft1 + (aft2 / l));
-
 		printf("%lu", aft2 % l);
-
-		aft1 = aft1 + bef1;
-
-		bef1 = aft1 - bef1;
-
+		aft1 = aft1 + bef;
+		bef1 = aft1 - bef1
 		aft2 = aft2 + bef2;
-
 		bef2 = aft2 - bef2;
-
 	}
-
 	printf("\n");
 
 	return (0);
-
 }
