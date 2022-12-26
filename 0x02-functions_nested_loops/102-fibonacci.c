1b56b36a3598b5a5be1c4737f4a817fb30c7a3ec
PeterGka <kymnjoro254@gmail.com>
2022-12-26 06:09:08 -0800

    Task 14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
+
+0;276;0c#include <stdio.h>
 
 /**
  * main - Prints first 50 Fibonacci numbers, starting with 1 and 2,
  *        separated by a comma followed by a space.
  *
- * Return: Always 
+ * Return: Always
  */
 
 int main(void)
