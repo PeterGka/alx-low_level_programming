1e9e7a26594b0c8c6153fce110608a636188badd
PeterGka <kymnjoro254@gmail.com>
2022-12-27 11:08:44 -0800

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -28,21 +28,15 @@ void times_table(void)
 			{
 
 				_putchar(',');
-
 				_putchar(' ');
-
 				_putchar(' ');
-
 				_putchar(k + '0');
 			}
 			else
 				if (k >= 10)
 				{
-
 				_putchar(',');
-
 				_putchar(' ');
-
 				_putchar((k / 10) + '0');
 				_putchar((k % 10) + '0');
 				}
