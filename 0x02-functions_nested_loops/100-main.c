c546494d49a46ac1f20eb95c06c38c2aedbe8006
PeterGka <kymnjoro254@gmail.com>
2022-12-27 11:17:58 -0800

    Task 12

--- 0x02-functions_nested_loops/100-main.c
+++ 0x02-functions_nested_loops/100-main.c
@@ -1,15 +1,9 @@
 #include "main.h"
 
-
-
 /**
-
  * main - check the code.
-
  *
-
  * Return: Always 0.
-
  */
 
 int main(void)
@@ -17,17 +11,11 @@ int main(void)
 {
 
 	print_times_table(3);
-
 	_putchar('\n');
-
 	print_times_table(5);
-
 	_putchar('\n');
-
 	print_times_table(98);
-
 	_putchar('\n');
-
 	print_times_table(12);
 
 	return (0);
