67500a003af116e6e9c89e2284bbf2a4e62d9b42
PeterGka <kymnjoro254@gmail.com>
2022-12-27 10:21:09 -0800

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,23 +4,15 @@
  */
 
 void times_table(void)
-
 {
 
 	int i, j, k;
 
-
-
-
-
 	for (i = 0; i < 10; i++)
-
 	{
-
 		for (j = 0; j < 10; j++)
 
 		{
-
 			k = j * i;
 
 			if (j == 0)
@@ -30,10 +22,6 @@ void times_table(void)
 				_putchar(k + '0');
 
 			}
-
-
-
-
 
 			if (k < 10 && j != 0)
 
@@ -58,10 +46,8 @@ void times_table(void)
 				_putchar((k / 10) + '0');
 
 				_putchar((k % 10) + '0');
-
 			}
 		}
-
 		_putchar('\n');
 	}
 }
